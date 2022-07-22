bf25d0e2c46045742c9ded9a1a9745c8bb8e67a9
subin <xoxosu0305@naver.com>
2022-07-22 16:52:20 +0900

    b2309

--- backjoon/b2587.java
+++ backjoon/b2587.java
@@ -10,7 +10,6 @@ public class b2587 {
         int sum = 0;
         int avg = 0;
         
-
         for(int i=0; i<arr.length; i++) { 
             arr[i] = sc.nextInt();
             sum += arr[i];
